7a6006a0262aa9e0827fae1adc3f23d3bcca488b
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:24:38 +0100

    Update 20190130160719_create_articles.rb

--- blogging_app/db/migrate/20190130160719_create_articles.rb
+++ blogging_app/db/migrate/20190130160719_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles do |t|
-      t.belongs_to :user, index: true
-      t.string :title
+    create_table :articles do |t| # création d'un tableau article
+      t.belongs_to :user, index: true # un article est créé par un utilisateur
+      t.string :title # ajout de 2 colonnes
       t.text :content
-      t.timestamps
+      t.timestamps# création automatique de 2 colonnes : date de création et date de modification
     end
   end
 end
