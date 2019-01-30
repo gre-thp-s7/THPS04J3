393e8d13b21ed5b97812c1b931fb7960793110e1
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:28:31 +0100

    Update user.rb

--- blogging_app/app/models/user.rb
+++ blogging_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :articles
-	has_many :comments
-	has_many :likes
+	has_many :articles # un utilisateur peut écrire plusieurs articles
+	has_many :comments # un utilisateur peut écrire plusieurs commentaires
+	has_many :likes # un utilisateur peut liker plusieurs articles
 end
