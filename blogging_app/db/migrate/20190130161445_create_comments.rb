0dc8b6d1daba0bf6540e9dbf977c072482bde410
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:25:43 +0100

    Update 20190130161445_create_comments.rb

--- blogging_app/db/migrate/20190130161445_create_comments.rb
+++ blogging_app/db/migrate/20190130161445_create_comments.rb
@@ -1,10 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.belongs_to :users, index: true
+      t.belongs_to :users, index: true # un commentaire est écrit par un user sur un article
       t.belongs_to :articles, index: true
-      t.text :name
-      t.timestamps
+      t.text :name # on crée une colonne
+      t.timestamps # création automatique de 2 colonnes : date de création et date de modification
     end
   end
 end
+
