710a67035e47f82cceb0919eb51c9acc940d7818
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:26:31 +0100

    Update seeds.rb

--- blogging_app/db/seeds.rb
+++ blogging_app/db/seeds.rb
@@ -8,25 +8,25 @@
 require 'faker'
 
 User.destroy_all
-Article.destroy_all
+Article.destroy_all # On part sur une base propre
 Category.destroy_all
-
+# Création de 10 instances de la class CreateUsers afin de rajouter 10 user dans le tableau User
 10.times do
   user = User.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.email)
 end
-
+# Création de 5 instances de la class CreateCategories afin de rajouter 5 catégorie dans le tableau catégorie
 5.times do
   category = Category.create!(name: Faker::Beer.unique.name)
 end
-
+# Création de 10 instances de la class CreateArticles afin de rajouter 10 article dans le tableau article
 10.times do
   article = Article.create!(title: Faker::RickAndMorty.character, content: Faker::RickAndMorty.quote, user_id: rand(1..10), category_id: rand(1..5))
 end
-
+# Création de 15 instances de la class CreateComments afin de rajouter 15 commentaires dans le tableau comment
 15.times do
   commentaire = Comment.create!(name: Faker::TheITCrowd.unique.quote, users_id: rand(1..10), articles_id: rand(1..10))
 end
-
+# Création de 15 instances de la class Createlikes afin de rajouter 15 likes dans le tableau like
 15.times do
   like = Like.create!(users_id: rand(1..10), articles_id: rand(1..10))
 end
