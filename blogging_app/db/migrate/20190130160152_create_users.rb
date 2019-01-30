361560efea399a6c4fbc5d601e18fe3a3ad0f05b
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:24:07 +0100

    Update 20190130160152_create_users.rb

--- blogging_app/db/migrate/20190130160152_create_users.rb
+++ blogging_app/db/migrate/20190130160152_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
-      t.string :first_name
-      t.string :last_name 
+    create_table :users do |t| # création d'un tableau user
+      t.string :first_name 
+      t.string :last_name # ajout de 3 colonnes
       t.string :email
-      t.timestamps
+      t.timestamps # création automatique de 2 colonnes : date de création et date de modification
     end
   end
 end
