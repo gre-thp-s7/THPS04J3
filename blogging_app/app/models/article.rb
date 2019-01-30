56f01482948873779ea97b62e6c236a5d614bab3
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:27:07 +0100

    Update article.rb

--- blogging_app/app/models/article.rb
+++ blogging_app/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-    belongs_to :user, optional: true
-    belongs_to :category, optional: true
-    has_many :like
-    has_many :comment
+    belongs_to :user, optional: true # un article a été posté par un utilisateur
+    belongs_to :category, optional: true # un article a une catégorie
+    has_many :like # un article peux avoir plusieurs likes
+    has_many :comment # un article peux avoir plusieurs commentaires
 end
+
