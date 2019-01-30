dc182eae14945187c9ad898522550db2668ecd58
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:28:04 +0100

    Update like.rb

--- blogging_app/app/models/like.rb
+++ blogging_app/app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :user,  optional: true
-	belongs_to :article, optional: true
+    belongs_to :user,  optional: true # un like a été effectué par un utilisateur
+	belongs_to :article, optional: true # un like concerne un article
 end
