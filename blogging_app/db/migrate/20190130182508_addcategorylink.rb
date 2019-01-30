ee33edb7deda482fc8a83ef6fd49f448e1192e31
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-01-31 00:25:58 +0100

    Update 20190130182508_addcategorylink.rb

--- blogging_app/db/migrate/20190130182508_addcategorylink.rb
+++ blogging_app/db/migrate/20190130182508_addcategorylink.rb
@@ -1,5 +1,5 @@
 class Addcategorylink < ActiveRecord::Migration[5.2]
   def change
-    add_reference :articles, :category, foreign_key: true
+    add_reference :articles, :category, foreign_key: true # ajout de la foreign key du tableau categrory dans le tableau articles
   end
 end
